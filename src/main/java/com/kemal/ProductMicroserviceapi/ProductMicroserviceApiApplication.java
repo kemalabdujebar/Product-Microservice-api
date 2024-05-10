ce256fb96f80ee6aab90563e66acb1b5b0d4a582
kemalabdujebar <85251727+kemalabdujebar@users.noreply.github.com>
2024-05-10 06:57:17 +0300

    initial commit

---
+++ src/main/java/com/kemal/ProductMicroserviceapi/ProductMicroserviceApiApplication.java
@@ -0,0 +1,13 @@
+package com.kemal.ProductMicroserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductMicroserviceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductMicroserviceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kemal/ProductMicroserviceapi/ProductMicroserviceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kemal.ProductMicroserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMicroserviceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
